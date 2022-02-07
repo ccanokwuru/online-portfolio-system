35a3dd6bcd76d92565f716f27b92d13a96475dd4
Anokwuru Chisom <cccanoks@gmail.com>
2022-02-07 11:56:14 +0100

    out servr

--- server/src/routes/routes.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyPluginAsync } from "fastify";
-
-const worksRoute: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get("/root/*", async function (request, reply) {
-    return request.url
-  });
-};
-
-export default worksRoute;
